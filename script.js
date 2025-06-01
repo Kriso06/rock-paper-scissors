bf8768dfac5f16a1751a1259e1a805e592c7c2d6
Krishnika Goel <krishnika.goel2006@gmail.com>
2025-06-01 12:54:48 +0530

    Updated Foundation

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-let comp='';
-function compMove(){
+function game(player){
+    let comp='';
     let random=Math.random();
     if(random>=0 && random<1/3) {comp='Rock';}
     else if(random>=1/3 && random<2/3) {comp='Paper';}
     else {comp='Scissors';}
-    console.log(comp);
-}
-
-let res='';
-function rock(){
-    if(comp==='Rock') res='Tie.';
-    else if(comp==='Paper') res='Computer Won.';
-    else res='You Won.';
-    alert(res);
-}
-
-function paper(){
-    if(comp==='Rock') res='You Won.';
-    else if(comp==='Paper') res='Tie.';
+    res='';
+    if(player===comp) res='Tie.';
+    else if((player==='Rock' && comp==='Scissors')||(player==='Scissors' && comp==='Paper')||(player==='Paper' && comp==='Rock')) res='You Won.';
     else res='Computer Won.';
-    alert(res);
-}
-function scissors(){
-    if(comp==='Rock') res='Computer Won.';
-    else if(comp==='Paper') res='You Won.';
-    else res='Tie.';
     alert(res);
 }
 
